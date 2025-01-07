370eb5aa875e0c4c86b238bee2c7e80b12317ddd
KhaledNjim <160496984+KhaledNjim@users.noreply.github.com>
2025-01-07 23:12:34 +0100

    Updated login integration test

--- integration_test/scenarios/login_scenario.dart
+++ integration_test/scenarios/login_scenario.dart
@@ -2,6 +2,7 @@ import 'package:fluffychat/pages/chat_list/chat_list.dart';
 import 'package:fluffychat/pages/homeserver_picker/homeserver_picker_view.dart';
 import 'package:fluffychat/pages/twake_welcome/twake_welcome.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:patrol/patrol.dart';
 import '../base/base_scenario.dart';
 import '../robots/login_robot.dart';
 
@@ -13,23 +14,23 @@ class LoginScenario extends BaseScenario {
   final String password;
 
   LoginScenario(
-    super.$, {
-    required this.username,
-    required this.serverUrl,
-    required this.password,
-  });
+      super.$, {
+        required this.username,
+        required this.serverUrl,
+        required this.password,
+      });
   @override
   Future<void> execute() async {
     final loginRobot = LoginRobot($);
     await $.waitUntilVisible($(TwakeWelcome));
-    await expectViewVisible($(TwakeWelcome));
     await loginRobot.tapOnUseYourCompanyServer();
     await $.waitUntilVisible(
       $(HomeserverPickerView),
     );
     await loginRobot.enterServerUrl(serverUrl);
     await loginRobot.confirmServerUrl();
-
+    await $.native.tap(Selector(text: "Use without an account"));
+    await $.native.waitUntilVisible(Selector(resourceId: 'login'));
     await loginRobot.enterUsernameSsoLogin(username);
     await loginRobot.enterPasswordSsoLogin(password);
     await loginRobot.pressSignInSsoLogin();
