92eaced685a83a942a4a24b72bed9cedb1cf0144
KhaledNjim <KhaledNjim@users.noreply.github.com>
2025-01-08 10:22:59 +0000

    deploy: a86150ddf3fead86eec355f792ce8da306ebaefa

--- 4/flutter_service_worker.js
+++ 4/flutter_service_worker.js
@@ -719,8 +719,8 @@ const RESOURCES = {"flutter.js": "f31737fb005cd3a3c6bd9355efd33061",
 "assets/AssetManifest.json": "f4a0bb7f8580a979eb33cb819f86e9ec",
 "assets/AssetManifest.bin.json": "4058aa3b6cf990784da9cf4e529a9fbe",
 "auth.html": "753913b429e0217a2c870bdafd63d063",
-"index.html": "e26a6bcc3774ac83e7a870a306d8fdcc",
-"/": "e26a6bcc3774ac83e7a870a306d8fdcc",
+"index.html": "e316458b4b157dfab7bb0a69761d96dc",
+"/": "e316458b4b157dfab7bb0a69761d96dc",
 "manifest.json": "f3c8e18d38224185c23bbca2dc0e3f4a",
 "canvaskit/canvaskit.js": "5fda3f1af7d6433d53b24083e2219fa0",
 "canvaskit/canvaskit.js.symbols": "48c83a2ce573d9692e8d970e288d75f7",
@@ -737,7 +737,7 @@ const RESOURCES = {"flutter.js": "f31737fb005cd3a3c6bd9355efd33061",
 "favicon.png": "2e69080e35d3ca42d9e9f78474df7d20",
 "style.css": "f70d0365df109f5c620005d49af5d7c0",
 "version.json": "3f311e41d5d8a9386b896cabb9373980",
-"flutter_bootstrap.js": "1cb720923887774fb1f95ab27014fd72"};
+"flutter_bootstrap.js": "c5aa7863b08a8648acaf4ed9a20bd23d"};
 // The application shell files that are downloaded before a service worker can
 // start.
 const CORE = ["main.dart.js",
